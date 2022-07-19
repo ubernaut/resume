b645ebc22ca3aa50587825f260e94145275ffa95
cos <collin.schroeder@gmail.com>
2022-07-19 15:52:40 -0800

    added deps

--- convert.sh
+++ convert.sh
@@ -1,4 +1,8 @@
 #!/bin/#!/usr/bin/env bash
+
+#dependencies:
+#sudo apt install pandoc texlive-latex texlive-latex-extra pdftk
+
 pandoc -s cschroeder-resume.md -o cschroeder-resume.pdf;
 pandoc -s cschroeder-project-history.md -o cschroeder-project-history.pdf;
 pdftk cschroeder-resume.pdf cschroeder-project-history.pdf cat output cschroeder-resume-proj-hist.pdf
