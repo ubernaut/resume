c08cfad692052f3ebcd6baab27d8d5308cd7733c
cos <collin.schroeder@gmail.com>
2024-05-23 12:30:41 -0800

    added light/dark mode

--- js/app.js
+++ js/app.js
@@ -1,18 +1,28 @@
-        //  console.log("works");
-        //  window.addEventListener('load', () => {
-        //           registerSW();
-        //         });
+        window.toggleDark = () => {
+                let body = document.getElementById("body");
+                if(body.getAttribute("class")== "crt light"){
+                        body.setAttribute("class", "crt dark");
+                }else{
+                        body.setAttribute("class", "crt light");
+                }
+        }
+        
+         console.log("works");
+         window.addEventListener('load', () => {
+                  registerSW();
+                });
             
-        //         // Register the Service Worker
-        //         async function registerSW() {
-        //           if ('serviceWorker' in navigator) {
-        //             try {
-        //               await navigator
-        //                     .serviceWorker
-        //                     .register('serviceworker.js');
-        //             }
-        //             catch (e) {
-        //               console.log('SW registration failed');
-        //             }
-        //           }
-        //         }
+                // Register the Service Worker
+                async function registerSW() {
+                  if ('serviceWorker' in navigator) {
+                    try {
+                      await navigator
+                            .serviceWorker
+                            .register('serviceworker.js');
+                    }
+                    catch (e) {
+                      console.log('SW registration failed');
+                    }
+                  }
+                }
+        
