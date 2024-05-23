f90ea5294b1051d1686e69377a26d67967659863
cos <collin.schroeder@gmail.com>
2024-05-23 11:33:14 -0800

    added HTML rendered version

--- js/app.js
+++ js/app.js
@@ -1,18 +1,18 @@
-         console.log("works");
-         window.addEventListener('load', () => {
-                  registerSW();
-                });
+        //  console.log("works");
+        //  window.addEventListener('load', () => {
+        //           registerSW();
+        //         });
             
-                // Register the Service Worker
-                async function registerSW() {
-                  if ('serviceWorker' in navigator) {
-                    try {
-                      await navigator
-                            .serviceWorker
-                            .register('serviceworker.js');
-                    }
-                    catch (e) {
-                      console.log('SW registration failed');
-                    }
-                  }
-                }
+        //         // Register the Service Worker
+        //         async function registerSW() {
+        //           if ('serviceWorker' in navigator) {
+        //             try {
+        //               await navigator
+        //                     .serviceWorker
+        //                     .register('serviceworker.js');
+        //             }
+        //             catch (e) {
+        //               console.log('SW registration failed');
+        //             }
+        //           }
+        //         }
