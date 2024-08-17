ad26f1ec9046e1d29ab99cebf8be5fb76036b5dc
ubernaut <collin.schroeder@gmail.com>
2024-08-17 08:31:33 -0800

    changed directory structure and html output folder

--- convert.sh
+++ convert.sh
@@ -8,9 +8,9 @@ pandoc -s resume.md -o resume.pdf;
 pandoc -s employment-history.md -o employment-history.pdf;
 pandoc -s project-history.md -o project-history.pdf;
 pdftk resume.pdf employment-history.pdf project-history.pdf cat output resume-empl-proj-hist.pdf
+pdftk resume.pdf employment-history.pdf cat output resume-empl-hist.pdf
 
-
-cat prefix.html >index.html;
+cat docs/prefix.html >index.html;
 markdown resume.md>>index.html;
 markdown employment-history.md>>index.html;
 markdown project-history.md>>index.html;
@@ -21,3 +21,6 @@ grep -vwE "(<h2>geometry: margin=1cm</h2>)" outdex.html > index.html;
 rm outdex.html;
 
 echo '</div></body></html>'>>index.html;
+
+mv index.html docs;
+mv *.pdf output;
